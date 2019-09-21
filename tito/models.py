dcf6d181b22caf8342c4921d604847eda8ea18e1
thetronjohnson <kiranjohns369@gmail.com>
2019-09-21 15:31:31 +0530

    translator model

--- tito/models.py
+++ tito/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
+from multiselectfield import MultiSelectField
+from django.contrib.auth.models import User
 # Create your models here.
+
+LANG_CHOICE = (
+    ('en','english'),
+    ('ml','malayalam'),
+    ('hi','hindi'),
+    ('ta','tamil'),
+    ('kn','kannada'),
+    ('ur','urdu'),
+    ('ma','marati'),
+)
+
+class Translator(models.Model):
+    username = models.CharField(max_length=100, unique=True)
+    fullname = models.CharField(max_length=100)
+    reputation = models.PositiveIntegerField()
+    languages = MultiSelectField(min_choices=1,choices=LANG_CHOICE,default='')
