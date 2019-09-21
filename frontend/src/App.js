7ea0e52ff350eb2ca4bd5f3b38a79790d83f1257
abhinav <abhinavprasad791@gmail.com>
2019-09-21 18:47:32 +0530

    starter

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 //import UserData from './components/Userdata';
 //import User from './components/user'
+import MobileContainer from "./components/Home"
+import Home from "./components/Home"
 class App extends Component {
     render() {
         return (
-            <p>Hello World</p>
+            <MobileContainer />
         )
     }
 }
