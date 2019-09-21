9dd41e341e10c7dda4c5347aa144c154500b1f1e
Athul-CA <athul8720@gmail.com>
2019-09-21 14:27:35 +0530

    Tweaks

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 //import UserData from './components/Userdata';
-import User from './components/user'
+//import User from './components/user'
 class App extends Component {
     render() {
         return (
-            <User />
+            <p>Hello World</p>
         )
     }
 }
