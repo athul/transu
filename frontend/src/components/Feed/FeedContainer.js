42958a530775b30f8937ccbaa620ecda9b24fa4a
Bonnie Simon <bonniesimon@protonmail.com>
2019-09-22 01:44:53 +0530

    Changed to custom component rather than Feed in semantic

--- frontend/src/components/Feed/FeedContainer.js
+++ frontend/src/components/Feed/FeedContainer.js
@@ -1,22 +1,12 @@
 import React ,{Component}from 'react';
 import { Feed, Divider } from 'semantic-ui-react';
-
+import FeedItem from "./FeedItem"
 
 class FeedContainer extends Component{
 render(){
     return(
     <div>
-        
-        <feed>
-            <feed.event>
-                <feed.content>
-                    <feed.summary>
-                        <feed.user>User2049</feed.user>
-                    </feed.summary>
-                    <feed.meta>Hello, I'm 2049</feed.meta>
-                </feed.content>
-            </feed.event>
-        </feed>
+        <FeedItem/>
     </div> 
     )
 }
---
+++ frontend/src/components/Feed/FeedItem.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class FeedItem extends Component{
+    render(){
+        return(
+            <div className="feed-item-wrapper">
+                <h1 className="question">the language to be converted</h1>
+                <h3 className="user">random2049</h3>
+            </div>
+        )
+    }
+}
+
+export default FeedItem;
