6c4ddbbbb0a1553950bb4e619c62d49c229fc2ff
thetronjohnson <kiranjohns369@gmail.com>
2019-09-22 02:08:34 +0530

    urls

--- translator/urls.py
+++ translator/urls.py
@@ -21,7 +21,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('translate/',include('tito.urls'))
-    path('api/token/',TokenObtainPairView.as_view())
+    path('translate/',include('tito.urls')),
+    path('api/token/',TokenObtainPairView.as_view()),
     path('api/token/refresh',TokenRefreshView.as_view())
 ]
