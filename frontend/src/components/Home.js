416af974134d233c5cbaa2082705833ad16b5b52
Bonnie Simon <bonniesimon@protonmail.com>
2019-09-22 12:12:03 +0530

    Fixed home page

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -1,55 +1,56 @@
 
 import React, { Component } from 'react'
+import './CSS/home.css'
 
 class HomepageLayout extends Component{
   render(){
     return(
 
       
-                <section class="hero is-info is-fullheight">
-          <div class="hero-head">
-              <nav class="navbar">
-                  <div class="container">
-                      <div class="navbar-brand">
-                          <a class="navbar-item" href="../">
+                <section className="hero is-info is-fullheight">
+          <div className="hero-head">
+              <nav className="navbar">
+                  <div className="container">
+                      <div className="navbar-brand">
+                          <a className="navbar-item" href="../">
                               <img src="http://bulma.io/images/bulma-type-white.png" alt="Logo"/>
                           </a>
-                          <span class="navbar-burger burger" data-target="navbarMenu">
+                          <span className="navbar-burger burger" data-target="navbarMenu">
                               <span></span>
-                              <span></span>
+                              <span></span> 
                               <span></span>
                           </span>
                       </div>
-                      <div id="navbarMenu" class="navbar-menu">
-                          <div class="navbar-end">
-                              <span class="navbar-item">
-                                  <a class="button is-white is-outlined" href="#">
-                                      <span class="icon">
-                                          <i class="fa fa-home"></i>
+                      <div id="navbarMenu" className="navbar-menu">
+                          <div className="navbar-end">
+                              <span className="navbar-item">
+                                  <a className="button is-white is-outlined" href="#">
+                                      <span className="icon">
+                                          <i className="fa fa-home"></i>
                                       </span>
                                       <span>Home</span>
                                   </a>
                               </span>
-                              <span class="navbar-item">
-                                  <a class="button is-white is-outlined" href="#">
-                                      <span class="icon">
-                                          <i class="fa fa-superpowers"></i>
+                              <span className="navbar-item">
+                                  <a className="button is-white is-outlined" href="#">
+                                      <span className="icon">
+                                          <i className="fa fa-superpowers"></i>
                                       </span>
                                       <span>Examples</span>
                                   </a>
                               </span>
-                              <span class="navbar-item">
-                                  <a class="button is-white is-outlined" href="#">
-                                      <span class="icon">
-                                          <i class="fa fa-book"></i>
+                              <span className="navbar-item">
+                                  <a className="button is-white is-outlined" href="#">
+                                      <span className="icon">
+                                          <i className="fa fa-book"></i>
                                       </span>
                                       <span>Documentation</span>
                                   </a>
                               </span>
-                              <span class="navbar-item">
-                                  <a class="button is-white is-outlined" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
-                                      <span class="icon">
-                                          <i class="fa fa-github"></i>
+                              <span className="navbar-item">
+                                  <a className="button is-white is-outlined" href="https://github.com/dansup/bulma-templates/blob/master/templates/landing.html">
+                                      <span className="icon">
+                                          <i className="fa fa-github"></i>
                                       </span>
                                       <span>View Source</span>
                                   </a>
@@ -60,23 +61,29 @@ class HomepageLayout extends Component{
               </nav>
               </div>
 
-              <div class="hero-body">
-                  <div class="container has-text-centered">
-                      <div class="column is-6 is-offset-3">
-                          <h1 class="title">
+              <div className="hero-body">
+                  <div className="container has-text-centered">
+                      <div className="column is-6 is-offset-3">
+                          <h1 className="title">
                               Coming Soon
                           </h1>
-                          <h2 class="subtitle">
+                          <h2 className="subtitle">
                               $this is the best software platform for running an internet business. We handle billions of dollars every year for forward-thinking businesses around the world.
                           </h2>
-                          <div class="box">
-                              <div class="field is-grouped">
+                          <div className="box is-centered">
+                              <div className="field is-grouped is-centered">
                                   
-                                  <p>
-                                      <a class="button is-info">
+                                  {/* <p className="is-pulled-left">
+                                      <a className="button is-info has-text-centered">
                                           Register Now
                                       </a>
                                   </p>
+                                  <p className="is-pulled-right">
+                                      <a className="button is-info has-text-centered">
+                                          Register Now
+                                      </a>
+                                  </p> */}
+                                  <a class="button is-success is-centered"  style={{textAlign:`center`,margin:'0 auto'}} >Register</a>
                               </div>
                           </div>
                       </div>
