7875150e4d54128b5aec65bfdd030dccf4ac4403
Bonnie Simon <bonniesimon@protonmail.com>
2019-09-22 08:54:49 +0530

    feeditem

--- frontend/src/components/Feed/FeedContainer.js
+++ frontend/src/components/Feed/FeedContainer.js
@@ -1,13 +1,26 @@
 import React ,{Component} from 'react';
-import { Feed, Divider } from 'semantic-ui-react';
+//import { Feed, Divider } from 'semantic-ui-react';
+import axios from 'axios'
 import FeedItem from "./FeedItem"
 
 class FeedContainer extends Component{
+    state = {
+        FeedItem: []
+      }
+      componentDidMount() {
+        axios.get('http://localhost:8000/users/')
+          .then(res => {
+            this.setState({
+              FeedItem: res.data
+            })
+            console.log(res.data)
+          })
+      }
 render(){
     return(
     <div>
         <h1 style={{textAlign:'center'}}>Feed</h1>
-        <FeedItem/>
+        <FeedItem fdata={this.state.FeedItem}/>
         <FeedItem/>
 
     </div> 
--- frontend/src/components/Feed/FeedItem.js
+++ frontend/src/components/Feed/FeedItem.js
@@ -2,16 +2,23 @@ import React,{Component} from "react";
 import "../CSS/FeedItem.css"
 
 class FeedItem extends Component{
+const data=({fdata})=>
+let data=Array.from(fdata);
     render(){
         return(
+            {data.map((udata)=>(
             <div className="feed-item-wrapper">
+
                 <h1 className="question">the language to be converted</h1>
-                <h3 className="user">random2049</h3>
+                <h3 className="user">{udata.username}</h3>
                 <Tags/>
 
             </div>
         )
+        )}
     }
 }
+
+
 
 export default FeedItem;
